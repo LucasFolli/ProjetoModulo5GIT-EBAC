f41cd99a4999dc4175c7df6a4dcf7a64a647d16f
LucasFolli <contato_lucasalves@yahoo.com>
2022-02-26 00:42:49 -0300

    Projeto enviado para o GITHUB.

---
+++ PrimeiraClasse.java
@@ -0,0 +1,8 @@
+public class PrimeiraClasse {
+
+    public static void main(String[] args) {
+        // O comando abaixo exibe uma mensagem na tela (no caso, "Hello, Lucas").
+        System.out.println("Hello, Lucas");
+
+    }
+}
---
+++ RODAR.bat
@@ -0,0 +1,17 @@
+@echo off
+title Projeto Exercicio Módulo 5 BackEnd JAVA
+ver
+echo Copyright (C) Everybody
+echo.
+javac PrimeiraClasse.java
+echo.
+java PrimeiraClasse
+echo.
+@echo.
+@echo    ***********************************************
+@echo   ***                                           ***
+@echo   **  Lucas Alves Mota Folli - EBAC BackEnd JAVA **
+@echo   ***                                           ***
+@echo    ***********************************************
+@echo.
+pause
